5d835abb53a636aade3ebcb75f7d52661879d4b6
netoudi <netoudi@rocketmail.com>
2019-11-30 17:55:13 -0300

    Criando recurso genre

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,13 +6,13 @@ use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Genre extends Model
 {
     use SoftDeletes, Uuid;
 
     public $incrementing = false;
 
-    protected $fillable = ['name', 'description', 'is_active'];
+    protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = ['id' => 'string'];
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['namespace' => 'Api'], function () {
     Route::apiResource('categories', 'CategoryController');
+    Route::apiResource('genres', 'GenreController');
 });
