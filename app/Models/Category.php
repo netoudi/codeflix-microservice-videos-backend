a4eede37d05c1507cb26ca1571b72936085739cc
netoudi <netoudi@rocketmail.com>
2019-12-04 15:06:22 -0300

    Testando criação de categoria

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,5 +14,5 @@ class Category extends Model
 
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
-    protected $casts = ['id' => 'string'];
+    protected $casts = ['id' => 'string', 'is_active' => 'boolean'];
 }
--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -32,4 +32,26 @@ class CategoryTest extends TestCase
             $categoryKey
         );
     }
+
+    public function testCreate()
+    {
+        $category = Category::create(['name' => 'test_name']);
+        $category->refresh();
+
+        $this->assertEquals('test_name', $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue($category->is_active);
+
+        $category = Category::create(['name' => 'test_name', 'description' => null]);
+        $this->assertNull($category->description);
+
+        $category = Category::create(['name' => 'test_name', 'description' => 'test_description']);
+        $this->assertEquals('test_description', $category->description);
+
+        $category = Category::create(['name' => 'test_name', 'is_active' => true]);
+        $this->assertTrue($category->is_active);
+
+        $category = Category::create(['name' => 'test_name', 'is_active' => false]);
+        $this->assertFalse($category->is_active);
+    }
 }
