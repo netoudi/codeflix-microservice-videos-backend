e942a1a68814fa7a5bae50c0288ac754d602c44e
netoudi <netoudi@rocketmail.com>
2019-12-17 21:55:44 -0300

    Criando e testando métodos para excluir arquivos

--- app/Models/Traits/UploadFiles.php
+++ app/Models/Traits/UploadFiles.php
@@ -22,4 +22,24 @@ trait UploadFiles
     {
         $file->store($this->uploadDir());
     }
+
+    /**
+     * @param array $files
+     */
+    public function deleteFiles(array $files)
+    {
+        foreach ($files as $file) {
+            $this->deleteFile($file);
+        }
+    }
+
+    /**
+     * @param string|UploadedFile $file
+     */
+    public function deleteFile($file)
+    {
+        $filename = $file instanceof UploadedFile ? $file->hashName() : $file;
+
+        \Storage::delete("{$this->uploadDir()}/{$filename}");
+    }
 }
