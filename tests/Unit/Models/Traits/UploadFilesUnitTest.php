06d8ab029ffb0c49e808b30c7cb42d7c51f9e652
netoudi <netoudi@rocketmail.com>
2019-12-17 21:15:46 -0300

    Primeiro teste com upload de arquivos

---
+++ tests/Unit/Models/Traits/UploadFilesUnitTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Models\Traits;
+
+use Illuminate\Http\UploadedFile;
+use Tests\Feature\Stubs\Models\Traits\UploadFilesStub;
+use Tests\TestCase;
+
+class UploadFilesUnitTest extends TestCase
+{
+    /**
+     * @var UploadFilesStub
+     */
+    private $obj;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->obj = new UploadFilesStub();
+    }
+
+    public function testUploadFile()
+    {
+        \Storage::fake();
+        $file = UploadedFile::fake()->create('video.mp4');
+        $this->obj->uploadFile($file);
+
+        \Storage::assertExists("1/{$file->hashName()}");
+    }
+
+    public function testUploadFiles()
+    {
+        \Storage::fake();
+        $file1 = UploadedFile::fake()->create('video.mp4');
+        $file2 = UploadedFile::fake()->create('video.mp4');
+        $this->obj->uploadFiles([$file1, $file2]);
+
+        \Storage::assertExists("1/{$file1->hashName()}");
+        \Storage::assertExists("1/{$file2->hashName()}");
+    }
+}
